23d01641c9d9409e33af64873919557be2f053da
DJ Imoehl <dj.imoehl@gmail.com>
2024-04-10 19:55:02 -0600

    Adding Recipe and Ingredient classes

---
+++ src/models/Ingredient.js
@@ -0,0 +1,10 @@
+
+class Ingredient {
+    constructor(id, name, unit, amount, optional) {
+        this.id = id;
+        this.name = name;
+        this.unit = unit;
+        this.amount = amount;
+        this.optional = optional;
+    }
+}
---
+++ src/models/Recipe.js
@@ -0,0 +1,15 @@
+
+class Recipe {
+
+    constructor(id, name, story, ingredientList, directions){
+        this.id = id;
+        this.name = name;
+        this.story = story;
+        this.ingredientList = ingredientList;
+        this.directions = directions;
+    }
+
+
+}
+
+export default Recipe;
